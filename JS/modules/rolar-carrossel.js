6515a552dfdc0bfb1d72267f7af9d6ec72ca7a5c
Marcus Vinicius <marcusdevinicius027@gmail.com>
2025-04-12 12:36:05 -0300

    Finalizei animação do carrossel

--- JS/modules/rolar-carrossel.js
+++ JS/modules/rolar-carrossel.js
@@ -1,17 +1,56 @@
 export default function initRolarCarrossel() {
     const carrossel = document.querySelector('.carrossel');
     const leftArrow = document.querySelector('.seta-esquerda')
-    // carrossel.style.transform = 'translate3d(-200px, 0, 0)';
+    const rightArrow = document.querySelector('.seta-direita')
+    const pontinhosNav = document.querySelectorAll('.pontinho-carrossel');
+    console.log(pontinhosNav)
+    // carrossel.style.transform = 'translate3d(-2920px, 0, 0)';
 
 
     let deslocamento = 0;
-    leftArrow.addEventListener('click', moveLeft);
+    rightArrow.addEventListener('click', moveRight);
 
-    function moveLeft(event) {
-        if(event.target === leftArrow) {
-            carrossel.style.transform = 'translate3d(-200px, 0, 0)'
+    function moveRight(event) {
             deslocamento = deslocamento - 200;
-        }
+            carrossel.style.transform = `translate3d(${deslocamento}px, 0, 0)`
+            if(deslocamento < -2400) {
+                rightArrow.removeEventListener('click', moveRight);
+            }
+            leftArrow.addEventListener('click', moveLeft);
+            animaPontinhos(deslocamento);
         console.log(deslocamento)
+    }
+
+    
+    function moveLeft() {
+        deslocamento += 200;
+        carrossel.style.transform = `translate3d(${deslocamento}px, 0, 0)`;
+        console.log(deslocamento);
+        if(deslocamento === 0) {
+            leftArrow.removeEventListener('click', moveLeft);
+        }
+        rightArrow.addEventListener('click', moveRight);
+        animaPontinhos(deslocamento);
+
+    }
+
+    function animaPontinhos(desloc) {
+        if(desloc > -1200) {
+            pontinhosNav[0].classList.add('ativo');
+            pontinhosNav[1].classList.remove('ativo');
+            pontinhosNav[2].classList.remove('ativo');
+        }
+
+        if(desloc <= -1200 && desloc > -2400) {
+            pontinhosNav[0].classList.remove('ativo');
+            pontinhosNav[1].classList.add('ativo');
+            pontinhosNav[2].classList.remove('ativo');
+        }
+
+        if(desloc === -2400) {
+            pontinhosNav[0].classList.remove('ativo');
+            pontinhosNav[1].classList.remove('ativo');
+            pontinhosNav[2].classList.add('ativo');
+        }
     }
 } 
--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
 import initRolarCarrossel from "./modules/rolar-carrossel.js";
 
 initRolarCarrossel();
+
